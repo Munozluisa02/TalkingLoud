f73d5701ccc4572e7b317054c0871186a7b8cfa4
Luisa Muñoz <munozluisa643@gmail.comgit config --global user.email munozluisa643@gmail.comgit config --global user.email munozluisa643@gmail.com>
2021-11-02 16:31:20 -0500

    streamchat api initialized and components in preparation

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { StreamChat } from 'stream-chat';
+import { ChannelList, Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+const apiKey = 'wnfxtyv779wf';
+
+const client= StreamChat.getInstance(apiKey)
 
 const App = () => {
     return (
-        <div>
-
+        <div className= "app__wrapper">
+            <Chat client={client} theme ="team light">
+                <ChannelListContainer
+                
+                />
+                <ChanelContainer
+                
+                />
+            </Chat>
         </div>
-    )
+    );
 }
 
 export default App
