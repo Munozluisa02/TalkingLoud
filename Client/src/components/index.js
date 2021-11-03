9a11d3d3f88b0df0932d33b73d503966edc4a435
Luisa Muñoz <munozluisa643@gmail.comgit config --global user.email munozluisa643@gmail.comgit config --global user.email munozluisa643@gmail.com>
2021-11-03 16:41:11 -0500

    Team channel list component done to a 50%

--- Client/src/components/index.js
+++ Client/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as ChannelListcontainer } from './ChannelListContainer';
 export { default as ChannelContainer} from './ChannelContainer';
 export { default as ChannelSearch } from './ChannelSearch';
+export { default as TeamChannelList } from './TeamChannelList';
